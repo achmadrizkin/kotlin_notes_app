584f0f03cc07a8d92ca2ece94302a7761659ae1c
achmadrizkin <arizki.nf02@gmail.com>
2021-11-04 15:13:16 +0700

    Navigation success, and still working

--- app/src/main/java/com/example/kotlin_notes_app/HomeFragment.kt
+++ app/src/main/java/com/example/kotlin_notes_app/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
@@ -24,6 +25,15 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fabCreateNote.setOnClickListener {
+            replaceFragment(CreateNoteFragment.newInstance(),false)
+        }
     }
 
     companion object {
@@ -35,4 +45,14 @@ class HomeFragment : Fragment() {
                 }
             }
     }
+
+    fun replaceFragment(fragment:Fragment, istransition:Boolean){
+        val fragmentTransition = requireActivity().supportFragmentManager.beginTransaction()
+
+        if (istransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.replace(R.id.frameLayout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+    }
+
 }
